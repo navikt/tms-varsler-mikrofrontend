941528c995cb3af374ef605318ec591b6cfa6525
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-01-13 07:26:38 +0100

    Legger ut i dev miljø for test

--- .eslintrc.pre-commit.js
+++ .eslintrc.pre-commit.js
@@ -3,5 +3,8 @@
 module.exports = {
   extends: [".eslintrc.js"],
   rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
