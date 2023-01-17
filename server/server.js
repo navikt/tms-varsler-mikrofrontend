38fedea89eda7320df4652b036e4abcde724f3a4
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-01-17 10:22:03 +0100

    La til manglende server.js fil

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const basePath = "/tms-varsler";
+const buildPath = path.resolve(__dirname, "../dist");
+const expressStaticGzip = require("express-static-gzip");
+const server = express();
+const corsAllowedOrigin = process.env.CORS_ALLOWED_ORIGIN || "http://localhost:3000";
+
+server.use(cors({ origin: corsAllowedOrigin }));
+
+server.use(
+  basePath,
+  expressStaticGzip(buildPath, {
+    enableBrotli: true,
+    orderPreference: ["br"],
+  })
+);
+
+server.get(`${basePath}/internal/isAlive`, (req, res) => {
+  res.sendStatus(200);
+});
+
+server.get(`${basePath}/internal/isReady`, (req, res) => {
+  res.sendStatus(200);
+});
+
+server.listen(7500, () => console.log("Server listening on port 7500"));
