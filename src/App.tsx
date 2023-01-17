e8031753198121ebb9f27316f9ed549453677506
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-01-17 13:32:57 +0100

    Endrer css for å få app til å fylle hele siden ved inntrekk i tms-min-side

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import MainPage from "./components/main-page/MainPage";
+import style from "./App.module.css";
 import "@navikt/ds-css";
 
 function App() {
-  return <MainPage />;
+  return (
+    <main className={style.main}>
+      <div className={style.app}>
+        <MainPage />;
+      </div>
+    </main>
+  );
 }
 
 export default App;
