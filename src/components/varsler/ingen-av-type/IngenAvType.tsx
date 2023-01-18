0139f8ea88e399e8b40f52d3a15189806164e02d
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-01-18 14:19:11 +0100

    css tilpasninger av ingen varsler visning

--- src/components/varsler/ingen-av-type/IngenAvType.tsx
+++ src/components/varsler/ingen-av-type/IngenAvType.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BodyShort } from "@navikt/ds-react";
 import { useIntl } from "react-intl";
 import style from "./IngenAvType.module.css";
+import TidligereVarslerInngang from "../inngang-tidligere-varsler/TidligereVarslerInngang";
 
 const IngenAvType = ({ type }: { type: string }) => {
   const translate = useIntl();
@@ -14,6 +15,7 @@ const IngenAvType = ({ type }: { type: string }) => {
           ? translate.formatMessage({ id: "oppgave.ingen" })
           : translate.formatMessage({ id: "beskjed.ingen" })}
       </BodyShort>
+      <TidligereVarslerInngang />
     </div>
   );
 };
