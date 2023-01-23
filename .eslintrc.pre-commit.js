e662416088b4c4aa2accd5dc9ebe5d8f65dc5826
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-01-23 14:04:35 +0100

    Reverterte et forsøk på å sette fast høyde på siden til å benytte fellesløsning igjen

--- .eslintrc.pre-commit.js
+++ .eslintrc.pre-commit.js
@@ -2,9 +2,5 @@
 
 module.exports = {
   extends: [".eslintrc.js"],
-  rules: {
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-  },
+  rules: {},
 };
