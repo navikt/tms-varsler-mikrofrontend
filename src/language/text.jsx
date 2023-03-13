fbf1dcb64d62d6d7dd91fed417dc4beb3fb6df8c
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-03-13 13:08:09 +0100

    La til ny text.jsx språkfil som skal erstatte de gamle

---
+++ src/language/text.jsx
@@ -0,0 +1,57 @@
+export const text = {
+  varslerTittel: {
+    nb: "Varsler",
+    nn: "Varsel",
+    en: "Notifications",
+  },
+  oppgaverTittel: {
+    nb: "Oppgaver",
+    nn: "Oppgåver",
+    en: "Tasks",
+  },
+  oppgaveIngen: {
+    nb: "Du har ingen oppgaver",
+    nn: "Du har ingen oppgåver",
+    en: "You have no tasks",
+  },
+  beskjederTittel: {
+    nb: "Beskjeder",
+    nn: "Beskjeder",
+    en: "Messages",
+  },
+  beskjedIngen: {
+    nb: "Du har ingen beskjeder",
+    nn: "Du har ingen beskjeder",
+    en: "You have no messages",
+  },
+  oppgaveMaskertTekst: {
+    nb: "Du har fått en oppgave, logg inn med høyere sikkerhetsnivå for å se oppgaven.",
+    nn: "Du har fått ei oppgåve, logg inn med høgare sikkerheitsnivå for å sjå oppgåva.",
+    en: "You have a task, please log in with a higher security level to see the task.",
+  },
+  beskjedMaskertTekst: {
+    nb: "Du har fått en beskjed, logg inn med høyere sikkerhetsnivå for å se meldingen.",
+    nn: "Du har fått ein beskjed, logg inn med høgare sikkerheitsnivå for å sjå beskjeden.",
+    en: "You have a message, please log in with a higher security level to read the message.",
+  },
+  inngangTidligereVarsler: {
+    nb: "Tidligere varsler",
+    nn: "Tidlegare varsel",
+    en: "Past notifications",
+  },
+  arkiverKnapp: {
+    nb: "Arkiver",
+    nn: "Arkiver",
+    en: "Archive",
+  },
+  varslerTomListe: {
+    nb: "Du har ingen nye varsler",
+    nn: "Du har ingen nye varsel",
+    en: "You have no new notifications",
+  },
+  varslerTomListeIngress: {
+    nb: "Vi varsler deg når noe skjer",
+    nn: "Vi varslar deg når noko skjer",
+    en: "We will notify you when something happens.",
+  },
+};
