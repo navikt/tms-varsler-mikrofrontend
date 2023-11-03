62fc8c0ca4c59b14ca70f2f7c587319ac2512f4e
Atle Nordland <atle.nordland@nav.no>
2023-11-03 10:43:16 +0100

    msw

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,16 @@ import "./index.css";
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
+if (process.env.NODE_ENV === "development") {
+  const msw = await import("./mocks/browser");
+  await msw.worker.start({ onUnhandledRequest: "bypass" });
+  msw.worker.listHandlers().forEach((handler) => {
+    console.log(handler.info.header);
+  });
+}
+
 root.render(
   <React.StrictMode>
     <Mikrofrontend />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteMockServe } from "vite-plugin-mock";
-import { ConfigEnv } from "vite";
-import { UserConfigExport } from "vitest/config";
-import { rollupImportMapPlugin } from "rollup-plugin-import-map";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-import { terser } from "rollup-plugin-terser";
 import { resolve } from "path";
+import { rollupImportMapPlugin } from "rollup-plugin-import-map";
+import { terser } from "rollup-plugin-terser";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 const reactUrl = "https://www.nav.no/tms-min-side-assets/react/18/esm/index.js";
 const reactDomUrl = "https://www.nav.no/tms-min-side-assets/react-dom/18/esm/index.js";
@@ -16,15 +12,11 @@ const imports = {
   "react-dom": reactDomUrl,
 };
 
-export default ({ command }: ConfigEnv): UserConfigExport => ({
+export default () => ({
   plugins: [
     react(),
     terser(),
     cssInjectedByJsPlugin(),
-    viteMockServe({
-      mockPath: "mock",
-      localEnabled: command === "serve",
-    }),
     {
       ...rollupImportMapPlugin([{ imports }]),
       enforce: "pre",
@@ -33,6 +25,7 @@ export default ({ command }: ConfigEnv): UserConfigExport => ({
   ],
   build: {
     manifest: true,
+    copyPublicDir: false,
     rollupOptions: {
       input: {
         "tms-varsler-mikrofrontend": resolve(__dirname, "src/Mikrofrontend.tsx"),
